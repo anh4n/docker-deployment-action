3151fbc1a7bdb0327146513bc319b6b4a0a6fe20
anh4n <anh4n@online.de>
2023-05-08 22:47:25 +0200

    check for missing certs

--- create-certs.sh
+++ create-certs.sh
@@ -1,3 +1,8 @@
+if [[ -z "$CA_PEM" || -z "$CERT_PEM" || -z "$KEY_PEM" ]]; then
+  echo "ca.pem, cert.pem or key.pem missing!";
+  exit 1;
+fi
+
 echo "$CA_PEM" > ca.pem \
 && echo "$CERT_PEM" > cert.pem \
 && echo "$KEY_PEM" > key.pem
